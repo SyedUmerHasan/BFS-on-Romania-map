ea0f96704c925fc9aa64a51e8d358aefe4199521
syedumerhasan <umerhasan2010@gmail.com>
2019-11-17 13:42:02 +0500

    complete

--- BFS.py
+++ BFS.py
@@ -100,6 +100,121 @@ def dfs_iterative(graph, start):
     return path
 
 
+def organizingArray(graph):
+    # adjacency_list = {
+    #     'A': [('B', 1), ('C', 3), ('D', 7)],
+    #     'B': [('D', 5)],
+    #     'C': [('D', 12)]
+    # }
+    tempDictionary = {}
+    for eachNode in graph:
+        eachArray = []
+        for combine in range(int(len(eachNode["connectingCities"]))):
+            eachArray.append((str(eachNode["connectingCities"][combine]), eachNode["costOfConnectingCities"][combine]))
+        tempDictionary[str(eachNode["cityID"])] = eachArray
+    return tempDictionary
+
+
+class Graph:
+    # example of adjacency list (or rather map)
+    # adjacency_list = {
+    # 'A': [('B', 1), ('C', 3), ('D', 7)],
+    # 'B': [('D', 5)],
+    # 'C': [('D', 12)]
+    # }
+
+    def __init__(self, adjacency_list):
+        self.adjacency_list = adjacency_list
+
+    def get_neighbors(self, v):
+        return self.adjacency_list[v]
+
+    # heuristic function with equal values for all nodes
+    def h(self, n):
+        H = {
+            '0': 1,
+            '1': 1,
+            '2': 1,
+            '3': 1
+        }
+        return H[n]
+
+    def a_star_algorithm(self, start_node, stop_node):
+        # open_list is a list of nodes which have been visited, but who's neighbors
+        # haven't all been inspected, starts off with the start node
+        # closed_list is a list of nodes which have been visited
+        # and who's neighbors have been inspected
+        open_list = set([start_node])
+        closed_list = set([])
+
+        # g contains current distances from start_node to all other nodes
+        # the default value (if it's not found in the map) is +infinity
+        g = {}
+
+        g[start_node] = 0
+
+        # parents contains an adjacency map of all nodes
+        parents = {}
+        parents[start_node] = start_node
+
+        while len(open_list) > 0:
+            n = None
+
+            # find a node with the lowest value of f() - evaluation function
+            for v in open_list:
+                if n == None or g[v] + self.h(v) < g[n] + self.h(n):
+                    n = v;
+
+            if n == None:
+                print('Path does not exist!')
+                return None
+
+            # if the current node is the stop_node
+            # then we begin reconstructin the path from it to the start_node
+            if n == stop_node:
+                reconst_path = []
+
+                while parents[n] != n:
+                    reconst_path.append(n)
+                    n = parents[n]
+
+                reconst_path.append(start_node)
+
+                reconst_path.reverse()
+
+                print('Path found: {}'.format(reconst_path))
+                return reconst_path
+
+            # for all neighbors of the current node do
+            for (m, weight) in self.get_neighbors(n):
+                # if the current node isn't in both open_list and closed_list
+                # add it to open_list and note n as it's parent
+                if m not in open_list and m not in closed_list:
+                    open_list.add(m)
+                    parents[m] = n
+                    g[m] = g[n] + weight
+
+                # otherwise, check if it's quicker to first visit n, then m
+                # and if it is, update parent data and g data
+                # and if the node was in the closed_list, move it to open_list
+                else:
+                    if g[m] > g[n] + weight:
+                        g[m] = g[n] + weight
+                        parents[m] = n
+
+                        if m in closed_list:
+                            closed_list.remove(m)
+                            open_list.add(m)
+
+            # remove n from the open_list, and add it to closed_list
+            # because all of his neighbors were inspected
+            open_list.remove(n)
+            closed_list.add(n)
+
+        print('Path does not exist!')
+        return None
+
+
 if __name__ == '__main__':
     # arrayOfDictionary = [
     #     {'cityID': 0, 'nameOfCity': 'Oradea', 'connectingCities': [1, 2], "costOfConnectingCities": [71, 151]},
@@ -116,3 +231,27 @@ if __name__ == '__main__':
     print("**********************************************************")
     dfspath = dfs_iterative(newarray, 0)
     print("The Path Followed = ", dfspath)
+    print("**********************************************************")
+    print("A* Search Algorithm")
+    print("**********************************************************")
+    # adjacency_list = {
+    #     '0': [('1', 71), ('2', 151)],
+    #     '1': [('3', 75)],
+    #     '2': [('3', 140)],
+    #     '3': [('1', 75), ('2', 140)]
+    # }
+    # print("======================")
+    # print(organizingArray(newarray))
+    # print("======================")
+    adjacency_list = organizingArray(newarray)
+    graph1 = Graph(adjacency_list)
+    # Please chnage the values to find after adding
+    path = graph1.a_star_algorithm('0', '3')
+
+    A_Star_Cost = 0
+    for i in path:
+        A_Star_Cost = A_Star_Cost + costArray[int(i)]
+
+    print("Total Cost for A Star = " , A_Star_Cost)
+
+    print("The best Algorithm is A* search")
