86642d61f518ebc0ea4e6dcf94d8476a4a86868b
syedumerhasan <umerhasan2010@gmail.com>
2019-11-17 11:37:04 +0500

    cost being calculated

--- BFS.py
+++ BFS.py
@@ -1,24 +1,42 @@
 import collections
 
+costArray = []
+
+def filterByCityID(cityId, graph):
+    iteratingArray = list(filter(lambda person: person['cityID'] == cityId, graph))
+    iteratingArray = iteratingArray[0]
+    return iteratingArray
 
 def new_bfs(graph, root):
-    temp = []
+
     for i in range(len(graph)):
-        temp.append(0)
+        costArray.append(0)
+
     visited, queue = set(), collections.deque([graph[root]["cityID"]])
     visited.add(graph[root]["cityID"])
     count = 0
     while queue:
         count += 1
+        # print("Iterating over Index ->", graph.)
         vertex = queue.popleft()
+        iteratingArray = filterByCityID(vertex, graph)
+        print("While " , iteratingArray)
+        count = 0
         for neighbour in graph[vertex]["connectingCities"]:
-            print(neighbour)
+            # neighbour = graph[vertex]["connectingCities"][count]
+            eachNeighbour = filterByCityID(neighbour, graph)
+            print("For",eachNeighbour)
             if neighbour not in visited:
+                costArray[eachNeighbour["cityID"]] = costArray[iteratingArray["cityID"]] + iteratingArray["costOfConnectingCities"][count]
+                print("cost ",iteratingArray["cityID"] ,"   " , iteratingArray["costOfConnectingCities"][count])
                 visited.add(neighbour)
-                print("Inside > " ,neighbour)
+                # print("IF Condition -> Inside > " ,neighbour)
                 queue.append(neighbour)
-    print(visited)
+            count += 1
 
+        print("", end="\n")
+
+    print(costArray)
 
 def input_map():
     nameConditionOfNodes = input("Do you want to enter city details? Yes/No (No for Default Map) ?")
@@ -47,10 +65,10 @@ def input_map():
     else:
         print("*********************Using Default City Map*********************")
         arrayOfDictionary = [
-            {'cityID': 0, 'nameOfCity': 'umer', 'connectingCities': [1, 2], "costOfConnectingCities": [10, 15]},
-            {'cityID': 1, 'nameOfCity': 'saba', 'connectingCities': [0], "costOfConnectingCities": [10, 15]},
-            {'cityID': 2, 'nameOfCity': 'sara', 'connectingCities': [3], "costOfConnectingCities": [10, 15]},
-            {'cityID': 3, 'nameOfCity': 'Huzaifa', 'connectingCities': [1, 2], "costOfConnectingCities": [10, 15]}]
+            {'cityID': 0, 'nameOfCity': 'Oradea', 'connectingCities': [1, 2], "costOfConnectingCities": [71, 151]},
+            {'cityID': 1, 'nameOfCity': 'Zerind', 'connectingCities': [3], "costOfConnectingCities": [75]},
+            {'cityID': 2, 'nameOfCity': 'Sibiu', 'connectingCities': [3], "costOfConnectingCities": [140]},
+            {'cityID': 3, 'nameOfCity': 'Arad', 'connectingCities': [1, 2], "costOfConnectingCities": [75, 140]}]
     return arrayOfDictionary
 
 
--- venv/bin/activate.csh
+++ venv/bin/activate.csh
@@ -8,7 +8,7 @@ alias deactivate 'test $?_OLD_VIRTUAL_PATH != 0 && setenv PATH "$_OLD_VIRTUAL_PA
 # Unset irrelevant variables.
 deactivate nondestructive
 
-setenv VIRTUAL_ENV "/home/syedumerhasan/Desktop/ArtificialIntelligence/Assignment1/venv"
+setenv VIRTUAL_ENV "/home/syedumerhasan/PycharmProjects/BFS-on-Romania-map/venv"
 
 set _OLD_VIRTUAL_PATH="$PATH"
 setenv PATH "$VIRTUAL_ENV/bin:$PATH"
